6bf79dfe29e9ce6b6b414b2eede1ef54538ec3e1
M1NA857 <ezgaming31@gmail.com>
2022-09-13 14:11:00 +0700

    GD

---
+++ app/src/main/java/com/example/view_binding_10698/TaskList.kt
@@ -0,0 +1,13 @@
+package com.example.view_binding_10698
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun",
+            "Keseharian yang membuat hari makin indah"),
+        Task("Chapter 2", "Live Stream", "Menjadi Anime untuk menghibur"),
+        Task("Chapter 3", "Turu", "Turu Terbaik"),
+        Task("Chapter 4", "evaluasi Live Stream", "Untuk Menjadi Lebih baik"),
+        Task("Chapter 5", "Mencari Clip", "untuk di upload ke sosmed dan mendapatkan viewer baru")
+    )
+
+}
